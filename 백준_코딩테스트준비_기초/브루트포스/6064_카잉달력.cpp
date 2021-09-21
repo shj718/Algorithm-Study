377bd924db923b9a51d8c65ddf1c2542badd28e9
서혜준 <90256209+shj718@users.noreply.github.com>
2021-09-21 20:48:58 +0900

    Create 6064_카잉달력.cpp

---
+++ 백준_코딩테스트준비_기초/브루트포스/6064_카잉달력.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+	int c;
+	while (b) {
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	return a;
+}
+
+int solve(int numOfYears, int M, int N, int x, int y) {
+	int a = 0;
+	int i = 0;
+	while(a <= numOfYears) {
+		a = M * i + x;
+		if ((a - 1) % N + 1 == y)
+			return a;
+		i++;
+	}
+	return -1;
+}
+
+int main() {
+	int T, M, N, x, y;
+	int numOfYears;
+	cin >> T;
+	int* answer = new int[T];
+	for (int i = 0; i < T; ++i) {
+		cin >> M >> N >> x >> y;
+		numOfYears = M * N / gcd(M, N);
+		answer[i] = solve(numOfYears, M, N, x, y);
+	}
+	for (int i = 0; i < T; ++i) {
+		cout << answer[i] << '\n';
+	}
+    delete[] answer;
+	return 0;
+}
