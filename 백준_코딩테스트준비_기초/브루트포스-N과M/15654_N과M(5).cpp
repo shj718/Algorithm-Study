5fe497681c6e453d40c03bf023b1023996469a21
서혜준 <90256209+shj718@users.noreply.github.com>
2021-09-24 12:37:26 +0900

    Create 15654_N과M(5).cpp

---
+++ 백준_코딩테스트준비_기초/브루트포스-N과M/15654_N과M(5).cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define MAX 8
+using namespace std;
+
+vector<int> v;
+int N, M;
+int arr[MAX];
+bool visited[MAX];
+
+void print() {
+	for (int i = 0; i < v.size(); ++i)
+		cout << v[i] << ' ';
+	cout << '\n';
+}
+
+void dfs(int cnt) {
+	if (cnt == M) {
+		print();
+		return;
+	}
+	for (int i = 0; i < N; ++i) {
+		if (!visited[i]) {
+			visited[i] = true;
+			v.push_back(arr[i]);
+			dfs(cnt + 1);
+			v.pop_back();
+			visited[i] = false;
+		}
+	}
+}
+
+int main() {
+	cin >> N >> M;
+	for (int i = 0; i < N; ++i) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + N);
+	dfs(0);
+	return 0;
+}
