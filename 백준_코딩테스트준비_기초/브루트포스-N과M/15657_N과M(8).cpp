99eb8be76d27fb47ea0e4787ef2f1e8da711cdf5
서혜준 <90256209+shj718@users.noreply.github.com>
2021-09-24 12:41:40 +0900

    Create 15657_N과M(8).cpp

---
+++ 백준_코딩테스트준비_기초/브루트포스-N과M/15657_N과M(8).cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define MAX 8
+using namespace std;
+
+vector<int> v;
+int N, M;
+int arr[MAX];
+
+void print() {
+	for (int i = 0; i < v.size(); ++i)
+		cout << v[i] << ' ';
+	cout << '\n';
+}
+
+void dfs(int start, int cnt) {
+	if (cnt == M) {
+		print();
+		return;
+	}
+	for (int i = start; i < N; ++i) {
+		v.push_back(arr[i]);
+		dfs(i, cnt + 1);
+		v.pop_back();
+	}
+}
+
+int main() {
+	cin >> N >> M;
+	for (int i = 0; i < N; ++i) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + N);
+	dfs(0, 0);
+	return 0;
+}
