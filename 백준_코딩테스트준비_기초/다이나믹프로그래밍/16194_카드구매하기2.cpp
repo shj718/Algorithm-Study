1f670cdcc6387aeeb0f42010ba1f09944ab75a9c
서혜준 <90256209+shj718@users.noreply.github.com>
2021-10-01 20:37:25 +0900

    Create 16194_카드구매하기2.cpp

---
+++ 백준_코딩테스트준비_기초/다이나믹프로그래밍/16194_카드구매하기2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N;
+int d[1001]; //d[x]는 x개의 카드를 구매하기 위해 지불해야 하는 금액의 최솟값
+int price[10001]; //price[y]는 카드가 y개 포함된 카드팩의 가격
+
+void dp() {
+	for (int i = 1; i <= N; ++i) {
+		d[i] = price[i];
+		for (int j = 1; j <= i; ++j) {
+			d[i] = min(d[i], d[i - j] + price[j]);
+		}
+	}
+}
+
+int main() {
+	cin >> N;
+	for (int i = 1; i <= N; ++i) {
+		cin >> price[i];
+	}
+	dp();
+	cout << d[N] << '\n';
+	return 0;
+}
